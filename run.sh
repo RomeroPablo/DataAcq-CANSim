0c82d6c709f1e313b7d3dcca73c1f2980f7280c1
RomeroPablo <romeropablojr03@gmail.com>
2024-10-06 17:36:47 -0500

    Remove all

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source bin/activate
-sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
-echo "run : 'candump vcan0' to monitor virutal CAN output"
-
-python canSIM.py
