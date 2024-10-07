858cc85866a310cb366c18562cb28d858448224f
RomeroPablo <romeropablojr03@gmail.com>
2024-10-07 12:25:39 -0500

    init vCAN

--- run.sh
+++ run.sh
@@ -7,6 +7,12 @@ source venv/bin/activate
 # Install dependencies
 pip install -r requirements.txt
 
+# setup virtual can port
+sudo modprobe vcan
+sudo ip link add dev vcan0 type vcan
+sudo ip link set up vcan0
+echo "run : 'candump vcvan0' to monitor virtual CAN output"
+
 # Run the Python program
 python src/your_program.py
 
